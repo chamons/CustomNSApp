1e288948794d710c90549814968a866f6ac52892
Chris Hamons <chris.hamons@xamarin.com>
2021-09-24 10:26:25 -0500

    Custom NSApplication example

---
+++ Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using AppKit;
+using Foundation;
+using ObjCRuntime;
+
+namespace CustomNSApp
+{
+	[Register("CustomNSApplication")]
+	public class CustomNSApplication : NSApplication
+	{
+		public CustomNSApplication(IntPtr handle) : base(handle) { }
+
+		public override void SendEvent(NSEvent theEvent)
+		{
+			base.SendEvent(theEvent);
+			System.Console.WriteLine("SendEvent");
+		}
+	}
+
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.Main(args);
+		}
+	}
+}
